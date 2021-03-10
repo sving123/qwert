5ffb5da86315a6f8fbc3492a57320cd4dfbde82c
sving123 <ilya_brich@mail.ru>
2021-03-10 20:46:04 +0300

    add function MyMul()

--- program.cpp
+++ program.cpp
@@ -2,11 +2,12 @@
 #include "stdafx.h"
 #include <iostream>
 #include "funcs.h"
-uing namespace std;
+using namespace std;
 int main()
 {
 cout << MySum(10, 3) << endl;
 cout << MySub(10, 3) << endl;
+cout << MyMul(10, 3) << endl;
 system("pause");
 return 0;
 }
