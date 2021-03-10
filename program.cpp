bec230903d2a06c3c0c8ff4c50daf6f1ec0053a9
sving123 <ilya_brich@mail.ru>
2021-03-10 20:41:06 +0300

    “Snapshot3

--- program.cpp
+++ program.cpp
@@ -2,10 +2,11 @@
 #include "stdafx.h"
 #include <iostream>
 #include "funcs.h"
-using namespace std;
+uing namespace std;
 int main()
 {
 cout << MySum(10, 3) << endl;
 cout << MySub(10, 3) << endl;
+system("pause");
 return 0;
 }
